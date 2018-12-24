49be3341c99074e2ce236a0ba6855a65abc33667
UmairAliShah <umairali43230@gmail.com>
2018-12-24 02:07:21 -0800

    Update after_install_script.sh

--- after_install_script.sh
+++ after_install_script.sh
@@ -1,4 +1,8 @@
 #!bin/bash
- yum -y update
- curl --silent --location https://rpm.nodesource.com/setup_10.x | bash 
- yum install -y nodejs > /dev/null < /dev/null &
+ #These commands are for Linux AMI
+ #yum -y update
+ #curl --silent --location https://rpm.nodesource.com/setup_10.x | bash 
+ #yum install -y nodejs > /dev/null < /dev/null &
+ sudo apt-get update
+ sudo apt-get install nodejs
+ sudo apt-get install npm
