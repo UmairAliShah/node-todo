79d70773f78e37b49fe1c4bd71956ae9c20d7987
UmairAliShah <umairali43230@gmail.com>
2018-12-24 02:03:14 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://ec2-34-243-225-71.eu-west-1.compute.amazonaws.com:27017/meanstacktutorials',
+    remoteUrl : 'mongodb://:27017/meanstacktutorials',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
